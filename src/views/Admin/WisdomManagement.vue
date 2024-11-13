bbf260da0c0ab48c8698361f0e76d6d2a022787c
2oo1s <leejoowon0817@gmail.com>
2024-11-13 12:11:10 +0900

    [feat] #18 - 지혜 전체 목록 조회

--- src/views/Admin/WisdomManagement.vue
+++ src/views/Admin/WisdomManagement.vue
@@ -1,33 +1,35 @@
 <script>
-import {onMounted, ref} from "vue";
+import { onMounted, ref } from "vue";
 import SearchFilterModal from "@/components/SearchFilterModal.vue";
-import {SEARCH_FILTER_TYPES} from "@/constants/searchFilterTypes";
-import {ROUTES} from "@/router/routes";
-import {useRouter} from "vue-router";
+import { SEARCH_FILTER_TYPES } from "@/constants/searchFilterTypes";
+import { ROUTES } from "@/router/routes";
+import { useRouter } from "vue-router";
+import { formatDate2 } from "@/utils/format";
+import handleApiCall from '@/api/apiService';
 
 export default {
   name: "WisdomManagement",
+  methods: { formatDate2 },
   computed: {
     SEARCH_FILTER_TYPES() {
       return SEARCH_FILTER_TYPES
     }
   },
-  components: {SearchFilterModal},
+  components: { SearchFilterModal },
   setup() {
     const router = useRouter();
 
     const modalPopupStatue = ref(false);
-    const searchInput = ref();
+    const searchInput = ref("");
     const filterTypes = ref("");
     const selectedWarning = ref("경고 필터링");
     const selectedJob = ref("전체 직무");
 
-    const postingList = ref();
-    const postingCount = ref(0);
+    const wisdomList = ref();
+    const wisdomCount = ref(0);
 
-    const searchJopPosting = () => {
-      // todo API 구현 시 아래에 로직 구현 하기
-      console.log("검색어:", searchInput.value);
+    const searchWisdom = () => {
+      fetchWisdoms();
     }
 
     const onFilterClick = (filterType) => {
@@ -38,45 +40,47 @@ export default {
     const handleSelectFilter = (selected) => {
       if (selected.filterType === SEARCH_FILTER_TYPES.WARNING) {
         selectedWarning.value = selected.filterValue;
+        fetchWisdoms();
       }
 
       if (selected.filterType === SEARCH_FILTER_TYPES.JOB) {
         selectedJob.value = selected.filterValue;
+        fetchWisdoms();
       }
     }
 
-    const fetchPosts = () => {
-      const data = [
-        {
-          postId: 1,
-          postTitle: '시니어도 할 수 있는 Ghat GPT',
-          job: '서비스',
-          date: '2024.01.01',
-          userId: 'user01'
-        },
-        {
-          postId: 2,
-          postTitle: '시니어도 할 수 있는 EXCEL',
-          job: 'IT',
-          date: '2024.01.01',
-          userId: 'user02'
-        },
-      ];
+    const fetchWisdoms = async () => {
+      try {
+        const knowhowReport = (selectedWarning.value === '경고 필터링' || selectedWarning.value === '없음')
+          ? null
+          : (selectedWarning.value === '많은 순' ? 'desc' : 'asc');
 
-      postingList.value = data;
-      postingCount.value = postingList.value.length;
+        const response = await handleApiCall('get', '/admin/knowhow', null, {
+          params: {
+            knowhowTitle: searchInput.value,
+            knowhowJob: selectedJob.value === '전체 직무' ? null : selectedJob.value,
+            knowhowReport: knowhowReport,
+          }
+        });
+
+        wisdomList.value = response.data;
+        wisdomCount.value = wisdomList.value.length;
+
+        console.log(response.data)
+      } catch (error) {
+        console.error("fetchWisdoms API 호출 오류:", error);
+      }
     }
 
     onMounted(() => {
-      fetchPosts();
+      fetchWisdoms();
     })
 
-    const onMoveDetailPageClick = (postId) => {
-      console.log("postId:", postId);
+    const onMoveDetailPageClick = (knowhowId) => {
       router.push({
         name: ROUTES.WISDOM_EXPLORE_DETAIL.name,
-        params:{
-          id: postId
+        params: {
+          id: knowhowId
         },
         query: {
           delete: true
@@ -90,9 +94,9 @@ export default {
       filterTypes,
       selectedWarning,
       selectedJob,
-      postingList,
-      postingCount,
-      searchJopPosting,
+      wisdomList,
+      wisdomCount,
+      searchWisdom,
       onFilterClick,
       handleSelectFilter,
       onMoveDetailPageClick,
@@ -105,45 +109,41 @@ export default {
   <main class="body">
     <div class="header">지혜마당 관리</div>
     <div class="search-wrap">
-      <input
-          class="search-input"
-          placeholder="게시물 제목을 검색하세요"
-          type="text"
-          v-model="searchInput"
-          @keyup.enter="searchJopPosting"
-      >
+      <input class="search-input" placeholder="게시물 제목을 검색하세요" type="text" v-model="searchInput"
+        @keyup.enter="searchWisdom">
       <div class="filter-section">
-        <div class="filter-item"
-             @click="onFilterClick(SEARCH_FILTER_TYPES.WARNING)"
-             :style="{ color: selectedWarning === '경고 필터링' ? 'black' : '#024CAA' }">
+        <div class="filter-item" @click="onFilterClick(SEARCH_FILTER_TYPES.WARNING)"
+          :style="{ color: selectedWarning === '경고 필터링' ? 'black' : '#024CAA' }">
           {{ selectedWarning }}
         </div>
-        <div class="filter-item"
-             @click="onFilterClick(SEARCH_FILTER_TYPES.JOB)"
-             :style="{ color: selectedJob === '전체 직무' ? 'black' : '#024CAA' }">
+        <div class="filter-item" @click="onFilterClick(SEARCH_FILTER_TYPES.JOB)"
+          :style="{ color: selectedJob === '전체 직무' ? 'black' : '#024CAA' }">
           {{ selectedJob }}
         </div>
       </div>
-      <div class="job-posting-wrap">
-        <div class="job-posting-info">{{ postingCount }}건</div>
-        <div class="job-posting-list" v-for="posting in postingList" :key="posting">
-          <div class="course-title">{{ posting.postTitle }}</div>
-          <div class="course-subtitle">{{ posting.job }}</div>
+      <div class="job-wisdom-wrap">
+        <div class="job-wisdom-info">{{ wisdomCount }}건</div>
+        <div class="job-wisdom-list" v-for="wisdom in wisdomList" :key="wisdom.postId">
+          <div class="course-title">{{ wisdom.knowhowTitle }}</div>
+          <div class="course-subtitle">{{ wisdom.knowhowJob }}</div>
           <div class="course-schedule">
-            <div class="schedule-info">{{ posting.userId }} &nbsp;&nbsp;&nbsp; {{ posting.date }}</div>
-            <img src="@/assets/images/icons/rightarrows.png" class="right-arrow-icon" alt="Right Arrow Icon" @click="onMoveDetailPageClick(posting.postId)">
+            <div class="schedule-info">
+              <div class="info-item">{{ wisdom.userId }}</div>
+              <div class="info-item">신고횟수 : {{ wisdom.reportedCnt }}</div>
+              <div class="info-item">{{ formatDate2(new Date(Date.parse(wisdom.uploadDate))) }}</div>
+            </div>
+            <img src="@/assets/images/icons/rightarrows.png" class="right-arrow-icon" alt="Right Arrow Icon"
+              @click="onMoveDetailPageClick(wisdom.knowhowId)">
           </div>
         </div>
       </div>
     </div>
   </main>
-  <search-filter-modal
-      v-if="modalPopupStatue"
-      @close-modal="modalPopupStatue = false"
-      @select-filter="handleSelectFilter"
-      :filter-type="filterTypes"
-  />
+  <search-filter-modal v-if="modalPopupStatue" @close-modal="modalPopupStatue = false"
+    @select-filter="handleSelectFilter" :filter-type="filterTypes" />
 </template>
+
+
 
 <style scoped>
 .body {
@@ -156,8 +156,8 @@ export default {
 
 .header {
   display: flex;
-  justify-content: space-between; /* 두 요소를 양 끝에 배치 */
-  align-items: center; /* 세로 정렬을 가운데로 */
+  justify-content: space-between;
+  align-items: center;
   font-size: 24px;
   font-weight: bold;
   margin-bottom: 20px;
@@ -167,6 +167,7 @@ export default {
   margin-top: 15px;
   margin-bottom: 15px;
 }
+
 .search-input {
   font-size: 16px;
   width: 100%;
@@ -203,33 +204,39 @@ export default {
   margin-right: 0;
 }
 
-.job-posting-wrap {
+.job-wisdom-wrap {
   margin-top: 10px;
 }
 
-.job-posting-info {
+.job-wisdom-info {
   font-size: 20px;
   font-weight: bold;
+  margin-top: 15px;
   margin-left: 10px;
   margin-bottom: 10px;
 }
 
-.job-posting-list {
-  background-color: #D9D9D9;
+.job-wisdom-list {
+  background-color: #ffffff;
   border-radius: 15px;
-  padding: 15px;
+  padding: 20px;
   color: #000000;
   font-weight: bold;
-  margin-bottom: 5px;
+  margin-bottom: 15px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 }
 
 .course-title {
   font-size: 18px;
-  margin-right: auto; /* Ensures it stays on the left */
+  margin-bottom: 8px;
 }
 
 .course-subtitle {
-  margin-bottom: 5px;
+  margin-bottom: 12px;
+  color: #6c757d;
 }
 
 .course-schedule {
@@ -239,12 +246,38 @@ export default {
 }
 
 .schedule-info {
-  flex: 1;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+}
+
+.info-item {
+  margin-right: 20px;
+  font-size: 14px;
+  color: #6c757d;
+  font-weight: normal;
 }
 
 .right-arrow-icon {
   width: 20px;
   height: auto;
   cursor: pointer;
+  margin-left: 15px;
+}
+
+@media (max-width: 768px) {
+  .job-wisdom-list {
+    padding: 15px;
+  }
+  .course-title {
+    font-size: 16px;
+  }
+  .course-subtitle {
+    font-size: 14px;
+  }
+  .schedule-info {
+    font-size: 12px;
+  }
 }
 </style>
