d3c0e1d2c6ccb41914ab74fb7430690fb75057b2
2oo1s <leejoowon0817@gmail.com>
2024-11-26 11:54:32 +0900

    [feat] #93 - 인프라 관리자 대시보드 기능 구현

--- src/views/Admin/LogDashboardManagement.vue
+++ src/views/Admin/LogDashboardManagement.vue
@@ -1,32 +1,75 @@
 <script>
+import { fetchLogDashboard } from "@/api/services/adminServiece";
+
 export default {
-  name: "LogDashboardManagement"
-}
+  name: "LogDashboardManagement",
+  setup() {
+    const loadDashboard = async () => {
+      try {
+        const response = await fetchLogDashboard();
+        console.log(response.data);
+        if (response.data) {
+          // 대시보드 URL로 이동
+          window.location.href = response.data;
+        }
+      } catch (error) {
+        console.error("fetchLogDashboard API 호출 오류:", error);
+      }
+    };
+
+    return {
+      loadDashboard,
+    };
+  },
+};
 </script>
 
 <template>
   <main class="body">
-    <div class="header">
-      <div class="header-title">로그 대시보드 관리</div>
+    <div class="dashboard-container">
+      <img class="dashboard-image" src="@/assets/images/icons/dashboard.png" alt="Dashboard Icon" />
+      <button class="log-button" @click="loadDashboard">대시보드 보러가기</button>
     </div>
   </main>
 </template>
 
 <style scoped>
 .body {
-  flex: 1;
-  padding: 20px;
-  box-sizing: border-box;
-  background-color: #f8f9fa;
-  overflow-y: auto;
+  display: flex;                  
+  justify-content: center;       
+  align-items: center;           
+  height: 100vh;                  
+  background-color: #f8f9fa;     
+  padding: 20px;                 
+  box-sizing: border-box;         
 }
 
-.header {
-  display: flex;
-  justify-content: space-between; /* 두 요소를 양 끝에 배치 */
-  align-items: center; /* 세로 정렬을 가운데로 */
-  font-size: 24px;
-  font-weight: bold;
-  margin-bottom: 20px;
+.dashboard-container {
+  display: flex;                  
+  flex-direction: column;        
+  align-items: center;         
+}
+
+.dashboard-image {
+  width: 220px;                  
+  margin-bottom: 20px;            
+}
+
+.log-button {
+  width: 200px;                
+  padding: 15px;                 
+  background-color: #024CAA;      
+  color: white;                  
+  text-align: center;             
+  cursor: pointer;                
+  font-size: 16px;
+  font-weight: bold;             
+  border-radius: 8px;           
+  border: none;                 
+  transition: background-color 0.2s; 
+}
+
+.log-button:hover {
+  background-color: #023A86;      
 }
 </style>
