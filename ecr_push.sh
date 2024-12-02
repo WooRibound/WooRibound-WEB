2d9428fd23e6a000ca90a6acadc9fc0e9692112c
odls <138199219+Oodls@users.noreply.github.com>
2024-12-03 00:56:35 +0900

    Update ecr_push.sh

--- ecr_push.sh
+++ ecr_push.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-docker tag wooribound/web:latest "${REGISTRY_URI}/wooribound/wooribound:web-latest"
-docker push "${REGISTRY_URI}/wooribound/wooribound:web-latest"
+# Debug outputs
+docker images
+echo "Full tag: ${REGISTRY_URI}/wooribound:web-latest"
+
+# Try with explicit URL format
+docker tag wooribound/web:latest "${REGISTRY_URI}/wooribound:web-latest"
+docker images   # Check if tag was successful
+docker push "${REGISTRY_URI}/wooribound:web-latest"
