260c6d50e8688ba445add6f10a9e7e3c53a01b69
odls <138199219+Oodls@users.noreply.github.com>
2024-12-03 00:53:54 +0900

    Update ecr_push.sh

--- ecr_push.sh
+++ ecr_push.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-echo "Registry URI: ${REGISTRY_URI}"
-docker images
-docker tag wooribound/web:latest "${REGISTRY_URI}/wooribound:web-latest"
-docker push "${REGISTRY_URI}/wooribound:web-latest"
+docker tag wooribound/web:latest "${REGISTRY_URI}/wooribound/wooribound:web-latest"
+docker push "${REGISTRY_URI}/wooribound/wooribound:web-latest"
