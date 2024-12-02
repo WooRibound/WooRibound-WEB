973885568e881543cd800aabe9946f131a0aaf47
odls <leesj@odlsui-MacBookAir.local>
2024-12-02 23:51:08 +0900

    [fix] #84 - fix ecr_push.sh

--- ecr_push.sh
+++ ecr_push.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # ECR Push Script
-docker tag wooribound/server:latest ${ECR_URI}/wooribound:web-latest
+docker tag wooribound/web:latest ${ECR_URI}/wooribound:web-latest
 docker push ${ECR_URI}/wooribound:web-latest
